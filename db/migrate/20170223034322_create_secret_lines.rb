1050691397457d8c3972de5c8d82f96ae2e96dc8
Ersan Bozduman <ersan.bozduman@gmail.com>
2017-02-22 19:46:34 -0800

    Fixes the error condition caused by syntax error in Secret_line controller creation

--- db/migrate/20170223034322_create_secret_lines.rb
+++ db/migrate/20170223034322_create_secret_lines.rb
@@ -1,7 +1,7 @@
 class CreateSecretLines < ActiveRecord::Migration[5.0]
   def change
     create_table :secret_lines do |t|
-      t.text, :secret_text
+      t.text :secret_text
       t.references :user, foreign_key: true
 
       t.timestamps
